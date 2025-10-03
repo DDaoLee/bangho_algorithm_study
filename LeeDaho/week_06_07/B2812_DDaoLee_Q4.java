d12a66cadb032ef42e8df061259245c6ea73824f
DDaoLee <dlekgh3576@naver.com>
2025-10-03 22:01:55 +0900

    chore:B2812_DDaoLee

--- LeeDaho/week_06_07/B2812_DDaoLee_Q4.java
+++ LeeDaho/week_06_07/B2812_DDaoLee_Q4.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class B2812_DDaoLee_Q5 {
+public class B2812_DDaoLee_Q4 {
     public static void main(String[] args) throws IOException {
         // 입력을 받기 위한 BufferedReader와 StringTokenizer
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
